953f673addc41d9090c0e240bce7d7cf8576912b
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-10-07 23:57:16 -0300

    Otimizando o código js.

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,22 +1,14 @@
-const lista_botoes = document.getElementsByClassName("botao");
-const lista_personagens = document.getElementsByClassName("personagem");
+const lista_botoes = document.querySelectorAll(".botao");
+const lista_personagens = document.querySelectorAll(".personagem");
 
-for (let i in lista_botoes) {
-    lista_botoes[i].setAttribute("id", `btn-${i}`);
-    lista_personagens[i].setAttribute("id", `personagem-${i}`);
-    lista_botoes[i].addEventListener("click", () => addSelecionado(`btn-${i}`));
-}
+lista_botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        const botao_selecionado = document.querySelector(".botao.selecionado");
+        botao_selecionado.classList.remove("selecionado");
+        botao.classList.add("selecionado");
 
-function addSelecionado(id) {
-    for (let i in lista_botoes) {
-        lista_botoes[i].classList.remove("selecionado");
-        lista_personagens[i].classList.remove("selecionado");
-
-        if (`btn-${i}` == id) {
-            document.getElementById(`btn-${i}`).classList.add("selecionado");
-            document
-                .getElementById(`personagem-${i}`)
-                .classList.add("selecionado");
-        }
-    }
-}
+        const personagem_selecionado = document.querySelector(".personagem.selecionado");
+        personagem_selecionado.classList.remove("selecionado");
+        lista_personagens[indice].classList.add("selecionado");
+    });
+});
